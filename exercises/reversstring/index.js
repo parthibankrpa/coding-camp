50e2d520b962a23fc2117e621aa070a3bc63e03f
Parthiban K <41236775+parthibankrpa@users.noreply.github.com>
2023-12-07 10:43:57 +0530

    multiple ways of reverse string added

--- exercises/reversstring/index.js
+++ exercises/reversstring/index.js
@@ -1,6 +1,24 @@
+// function reverse(str){
+
+//     let reversed = '';
+
+//     for(let character of str){
+//         reversed = character+reversed;
+//     }
+
+//     return reversed;
+//     // return str.split('').reverse().join('');
+// }
+
+// function reverse(str){
+
+//     return str.split('').reverse().join('');
+// }
 function reverse(str){
 
-    return str.split('').reverse().join('');
+    return str.split('').reduce((reversed,character)=>character+reversed);
+
 }
+
 
 module.exports = reverse;
